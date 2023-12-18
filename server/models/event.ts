dd5407e21ca83f452dc4c4fe6b7b5820dadc6345
Rakoski <mastrakoski@gmail.com>
2023-12-18 06:45:56 -0300

    Defined my schema for an event
    Using mongoose and model definitions, I defined a single schema for an event in a separate event.ts file

---
+++ server/models/event.ts
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from 'mongoose';
+
+const schema = mongoose.Schema
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+});
+
+mongoose.model('Event', eventSchema;
